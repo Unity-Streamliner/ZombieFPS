f480961ae64c6a8e84e2da336113f18ad92bffc7
tiger <thisisstreamliner@gmail.com>
2023-01-23 23:43:18 +0200

    Implement Shoot

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
+        print($"I hit this thing {hit}");
+    }
+}

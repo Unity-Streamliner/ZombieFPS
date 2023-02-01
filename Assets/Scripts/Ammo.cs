b9a6265b934344bd8084b76b60d4e9f5773ee138
tiger <thisisstreamliner@gmail.com>
2023-02-01 17:28:21 +0200

    Basic Ammo Functionality (Addition)

---
+++ Assets/Scripts/Ammo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+    [SerializeField] int amount = 10;
+
+    public int GetCurrentAmmo()
+    {
+        return amount;
+    }
+    
+    public void ReduceAmmo()
+    {
+        amount--;
+    }
+}
